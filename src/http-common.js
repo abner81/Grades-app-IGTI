9c664e931a221ea4d55bff8fbee234539800a81b
Abner Machado <abner81@live.com>
2020-07-11 15:52:04 -0300

    final

--- src/http-common.js
+++ src/http-common.js
@@ -2,8 +2,8 @@ import axios from 'axios';
 
 //Define a URL base da origem para consumo do servico
 export default axios.create({
-  baseURL: 'http://localhost:8081/',
+  baseURL: "https://grades-api-backend-igti.herokuapp.com/",
   headers: {
-    'Content-type': 'application/json',
+    "Content-type": "application/json",
   },
 });
